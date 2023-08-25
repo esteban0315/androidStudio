5e2bb7198b4d5bd4860b77acd6f9feaf2c586f2a
esteban <estebanvillz@hotmail.com>
2023-08-25 11:01:24 -0500

    primer proyecto en Android Studio

---
+++ app/src/main/java/com/evzapp/proyecto1/Variables.kt
@@ -0,0 +1,20 @@
+package com.evzapp.proyecto1
+
+
+/**
+ * VARIABLES Y VALORES
+ * cuando creas e instancías una variables, está es modificable
+ * pero un valor no es modificable
+ * para crear una variables se utiliza la palabra reservada val
+ */
+
+
+
+fun main(){
+    //Variables númericas
+    // Int - desde -2,147,483,647 a 2,147,483,647
+    val age:Int = 30
+    //Long - desde -9,223,372,036,854,775,807 a 9,223,372,036,854,775,807
+    val example:Long = 30
+
+}
