5d0f45ecd1dc4eda81314206febf366bc9c5d5c2
esteban <estebanvillz@hotmail.com>
2023-08-25 11:17:52 -0500

    Definición de variables

--- app/src/main/java/com/evzapp/proyecto1/Variables.kt
+++ app/src/main/java/com/evzapp/proyecto1/Variables.kt
@@ -5,16 +5,49 @@ package com.evzapp.proyecto1
  * VARIABLES Y VALORES
  * cuando creas e instancías una variables, está es modificable
  * pero un valor no es modificable
- * para crear una variables se utiliza la palabra reservada val
+ * para crear un valor se utiliza la palabra reservada val
+ * para crear una variables se utiliza la palabra reservada var
  */
 
 
 
 fun main(){
-    //Variables númericas
+    /**
+     * VARIABLES NÚMERICAS
+     */
+
     // Int - desde -2,147,483,647 a 2,147,483,647
     val age:Int = 30
+
     //Long - desde -9,223,372,036,854,775,807 a 9,223,372,036,854,775,807
     val example:Long = 30
+
+    //float - hasta 6 decimales
+    val exampleFloat:Float = 15.6f
+
+    //Double - soportan 14 decimales
+    val exampleDouble:Double = 30.4546253214455
+
+    /**
+     * VARIABLES ALFANUMÉRICAS
+     */
+
+    //Char
+    val charExample:Char = 'e'
+    val charExample1:Char = '2'
+    val charExample2:Char = '@'
+
+    //String
+    val stringExample:String = "Hola cómo vas"
+
+    /**
+     * VARIABLES BOOLEANAS
+     */
+
+    //Boolean
+    val booleanExample:Boolean = true
+    val booleanExample1:Boolean = false
+
+
 
 }
